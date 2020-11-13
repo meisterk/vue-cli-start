dce98cc29b6d39b6b76c767cfbaa3bbc1605e530
Franz Kohnle <franz@kohnlehome.de>
2020-11-13 12:33:45 +0100

    Initial commit

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ ueberschrift }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  props: {
+    ueberschrift: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  background-color: lightgrey;
+  border: 1px solid black;
+  padding: 0.5em;
+  text-align: center;
+}
+</style>
