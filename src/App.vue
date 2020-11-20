9210e38675ea781b24ffc956c7876cec9434b171
Franz Kohnle <franz@kohnlehome.de>
2020-11-20 14:51:51 +0100

    Add MyCounter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <img alt="Logo der GBS Schulen München" src="./assets/logo.png" />
   <MyComponent ueberschrift="Hallo!" />
+  <MyCounter ueberschrift="Zähler1" @gezaehlt="gezaehlt1" />
+  <MyCounter ueberschrift="Zähler2" @gezaehlt="gezaehlt2" />
+  <p>Gesamtzahl: {{ gesamtzahl }}</p>
   <MyForm />
   <MyComponent ueberschrift="Servus!" />
 </template>
@@ -8,12 +11,33 @@
 <script>
 import MyComponent from "./components/MyComponent.vue";
 import MyForm from "./components/MyForm.vue";
+import MyCounter from "./components/MyCounter.vue";
 
 export default {
   name: "App",
   components: {
     MyComponent,
     MyForm,
+    MyCounter,
+  },
+  data() {
+    return {
+      zahl1: 0,
+      zahl2: 0,
+    };
+  },
+  computed: {
+    gesamtzahl() {
+      return this.zahl1 + this.zahl2;
+    },
+  },
+  methods: {
+    gezaehlt1(zahl) {
+      this.zahl1 = zahl;
+    },
+    gezaehlt2(zahl) {
+      this.zahl2 = zahl;
+    },
   },
 };
 </script>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <h1>{{ ueberschrift }}</h1>
+    <button @click="hochzaehlen">Hochzählen</button>
+    <p>{{ zahl }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyCounter",
+  props: {
+    ueberschrift: String,
+  },
+  data() {
+    return {
+      zahl: 0,
+    };
+  },
+  methods: {
+    hochzaehlen() {
+      this.zahl++;
+      this.$emit("gezaehlt", this.zahl);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  background-color: yellow;
+  border: 1px solid black;
+  padding: 0.5em;
+  text-align: center;
+}
+</style>
